e5a129e5aef0f19360b5f927eef171f2ca3fa302
Jieyi-Jiang <2177083893@qq.com>
2025-02-04 23:12:15 +0800

    完成gui整体框架设计

--- MainAPP.py
+++ MainAPP.py
@@ -15,8 +15,12 @@ class MainWindow(Ui_MainWindow, QMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.retranslateUi(self)
-
+        self.image_source = None
+        self.image_gray = None
+        self.map_grid = None
+        self.map_dilate = None
+        self.image_search_path = None
+        self.image_smooth_path = None
 
 
 if __name__ == '__main__':
